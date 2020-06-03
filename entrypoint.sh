60ba4208cbe9334747e7682188904c2461bb3a83
Daniel Foley <daniel@foley.life>
2020-06-03 14:37:21 -0500

    updated entrypoint script to include; set +e instead of -e

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e
+# Setting the +e will tell the environment not to exit the script if a "failure" occurs. This will allow 
+# us to run the database query to check if the table exists, if not create it.
+set +e
 
 if [ -z "$DB_RESTORE_URL" ]; then
   DB_RESTORE_URL="https://gist.githubusercontent.com/jamster/7cd070880698efdd7828/raw"
@@ -14,6 +16,10 @@ else
   rake db:migrate
   if [ "$DB_RESTORE" == "true" ]; then
     echo "Database will be restored from backup..."
+
+    # I had to do something different to get the updated information to load
+    # it's possible I just didn't restore it the way it was meant to be. So
+    # leaving the command in here regardless for now.
     curl $DB_RESTORE_URL | PGPASSWORD=$DB_PASS psql --host $DB_HOST -U $DB_USER -d $DB
   else
     echo "Database will not be restored from backup..."
