2453a9f0a0a0e5af661ecf366e0d349b0762d98c
Daniel Foley <daniel@foley.life>
2020-06-03 01:02:49 -0500

    fixed bash logic for entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 set -e
 
 PGPASSWORD=$DB_PASS psql --host $DB_HOST -U $DB_USER -d $DB -c "SELECT 'public.links'::regclass"
-if $? ; then
+if [ $? -eq 0 ]; then
   echo "Database Already Loaded... proceeding"
 else
+  echo "Links table not found in database..."
   echo "Loading database; rake db:migrate..."
   rake db:migrate
 fi
